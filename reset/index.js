0d250c86d7bb1570b2435a575108a3bc686fc5f5
autumnFish <517729329@qq.com>
2019-08-04 21:18:32 +0800

    调整后台文章搜索数据接口，接口文档错误，增加所有状态

--- reset/index.js
+++ reset/index.js
@@ -44,9 +44,12 @@ db.sequelize.sync({ force: true }).then(async () => {
     await db.Comment.bulkCreate(randomData(8000).Comment)
     // console.log( randomData(10000).Comment.length)
     await db.User.create(userData)
-    await fs.copyFileSync(path.join(__dirname,'./static/icon.jpg'),path.join(__dirname,'../uploads/icon.jpg'))
+    fs.mkdirSync('./uploads')
+    fs.copyFileSync(path.join(__dirname,'./static/icon.jpg'),path.join(__dirname,'../uploads/icon.jpg'))
     // 移动图片
-    await console.log('初始化完毕！！');
+    setTimeout(() => {
+      console.log('搞定')
+    }, 0);
   } catch (error) {
     console.log(error)
   }
